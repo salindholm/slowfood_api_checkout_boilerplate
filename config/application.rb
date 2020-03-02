6f63f02bd46ed6248b02548921395f5422a80f45
Blake <blake@futchi.com>
2020-03-02 15:16:13 +0100

    setup

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require "rails"
-# Pick the frameworks you want:
+
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -12,11 +12,7 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module SlowfoodApi
@@ -32,6 +28,14 @@ module SlowfoodApi
           methods: %i[get post put delete],
           expose: %w(access-token expiry token-type uid client),
           max_age: 0
+      end
+      config.generators do |generate|
+        generate.helper false
+        generate.assets false
+        generate.view_specs false
+        generate.helper_specs false
+        generate.routing_specs false
+        generate.controller_specs false
       end
     end
   end
