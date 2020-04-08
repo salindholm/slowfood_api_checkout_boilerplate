286dd0d388a30225d20685f672808e71c5e184dc
faraznaeem <faraznaeem@gmail.com>
2020-04-08 15:31:59 +0200

    Refactors tests and debugs implementation code

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -4,4 +4,11 @@ class Api::OrdersController < ApplicationController
     order.order_items.create(product_id: params[:product_id])
     render json: { message: 'The product has been added to your order', order_id: order.id }
   end
+  
+  def update
+    order = Order.find(params[:id])
+    product = Product.find(params[:product_id])
+    order.order_items.create(product: product)
+    render json: { message: 'The product has been added to your order', order_id: order.id }
+  end
 end
--- spec/requests/api/client_can_create_new_order_spec.rb
+++ spec/requests/api/client_can_create_new_order_spec.rb
@@ -1,19 +1,34 @@
 RSpec.describe Api::OrdersController, type: :request do
-  let!(:product_1) { create(:product, name: "Pizza") }
-  let!(:product_2) { create(:product, name: "Kebab") }
-
-  before do
-    post "/api/orders", params: { id: product_1.id }
-    @order = Order.last
+  let!(:product_1) { create(:product, name: 'Pizza') }
+  let!(:product_2) { create(:product, name: 'Kebab') }
+  
+	before do
+		post '/api/orders', params: { product_id: product_1.id }
+		@order_id = JSON.parse(response.body)['order_id']
+  end
+  
+  describe 'POST /api/orders' do
+    it 'responds with success message' do
+      expect(JSON.parse(response.body)['message']).to eq 'The product has been added to your order'
+    end
+    it 'responds with order id' do
+      order = Order.find(@order_id)
+      expect(JSON.parse(response.body)['order_id']).to eq order.id
+    end
   end
 
-  it "responds with success message" do
-    post "/api/orders", params: { id: product_1.id }
-    expect(JSON.parse(response.body)["message"]).to eq "The product has been added to your order"
-  end
+  describe 'PUT /api/orders/:id' do
+    before do
+      put "/api/orders/#{@order_id}", params: { product_id: product_2.id }
+      @order = Order.find(@order_id)
+    end
 
-  it 'adds another product to order if param "order_id" is present' do
-    post '/api/orders', params: {id: product_2.id, order_id: @order.id}
-    expect(@order.order_items.count).to eq 2
+    it 'adds another product to order if request is a PUT and param id of the order is present' do
+      expect(@order.order_items.count).to eq 2
+    end
+
+    it 'responds with order id' do
+      expect(JSON.parse(response.body)['order_id']).to eq @order.id
+    end
   end
 end
