c065833be30a070c29754a57b388e75f2714029d
faraznaeem <faraznaeem@gmail.com>
2020-05-07 10:54:05 +0200

    Adds update action to controller and modifes and refactors spec

--- spec/requests/api/order_spec.rb
+++ spec/requests/api/order_spec.rb
@@ -1,11 +1,35 @@
 RSpec.describe Api::OrdersController, type: :request do
-  let!(:product_1) {create(:product, name: 'Pizza')}
-  let!(:product_2) {create(:product, name: 'Pasta')}
+  let!(:product_1) { create(:product, name: "Pizza") }
+  let!(:product_2) { create(:product, name: "Pasta") }
 
+  before do
+    post "/api/orders", params: { product_id: product_1.id }
+    @order_id = JSON.parse(response.body)["order_id"]
+  end
 
+  describe "POST /api/orders" do
+    it "responds with a success message" do
+      expect(JSON.parse(response.body)["message"]).to eq "The product has been added to your order"
+    end
 
-  it 'responds with a success message' do
-    post '/api/orders', params: {id: product_1.id}
-    expect(JSON.parse(response.body)['message']).to eq 'The product has been added to your order'
+    it "responds with order id" do
+      order = Order.find(@order_id)
+      expect(JSON.parse(response.body)["order_id"]).to eq order.id
+    end
+  end
+
+  describe "PUT /api/orders/:id" do
+    before do
+      put "/api/orders/#{@order_id}", params: { product_id: product_2.id }
+      @order = Order.find(@order_id)
+    end
+
+    it "adds another product to order" do
+      expect(@order.order_items.count).to eq 2
+    end
+
+    it "responds with order id" do
+      expect(JSON.parse(response.body)["order_id"]).to eq @order.id
+    end
   end
 end
