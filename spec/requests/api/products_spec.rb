46264c5f5ecf7049bd7a1cf3b98dfe4763223208
Sanne <sannelindholm@hotmail.com>
2020-11-24 13:15:06 +0100

    Refactoring naming in specs

--- spec/requests/api/products_spec.rb
+++ spec/requests/api/products_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe Api::ProductsController, type: :request do
       get '/api/products'
     end
 
-    it 'should return a 200 response' do
+    it 'is expected to return a 200 response' do
       expect(response).to have_http_status 200
     end
 
-    it 'should return three products' do
+    it 'is expected to return three products' do
       expect(JSON.parse(response.body)['products'].count).to eq 3
     end
   end
