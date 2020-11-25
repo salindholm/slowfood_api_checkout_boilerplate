e6fec2e2e5049493dbb4029c67c35dcb24ca6f5f
Sanne <sannelindholm@hotmail.com>
2020-11-25 18:38:40 +0100

    Adding user registrartion endpoint

---
+++ spec/requests/api/user_registration_endpoint_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe 'POST /api/auth/', type: :request do
+  describe 'with valid credentials' do
+    before do
+      post '/api/auth/',
+           params: {
+             email: 'thomas@craft.com',
+             password: 'password',
+             password_confirmation: 'password'
+           }
+    end
+
+    it {
+      expect(response).to have_http_status 200
+    }
+
+    it 'is expected to respond with a "status" message' do
+      expect(JSON.parse(response.body)['status']).to eq 'success'
+    end
+  end
+end
